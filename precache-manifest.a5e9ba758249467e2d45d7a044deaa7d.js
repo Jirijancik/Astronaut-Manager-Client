10916107ee83f03b9c2ecafb2fcd1c9f9c42de8f
jirijancik <jirijancik2@gmail.com>
2019-12-28 12:18:55 +0100

    Updates

---
+++ precache-manifest.a5e9ba758249467e2d45d7a044deaa7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6827afc0d6dfb8986c1af32cde525426",
+    "url": "/Astronaut-Manager-Client/index.html"
+  },
+  {
+    "revision": "b558aba44b7c941c355f",
+    "url": "/Astronaut-Manager-Client/static/css/main.d044a18f.chunk.css"
+  },
+  {
+    "revision": "293d396fe1ef9e0609e2",
+    "url": "/Astronaut-Manager-Client/static/js/2.93ee6dd5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Astronaut-Manager-Client/static/js/2.93ee6dd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b558aba44b7c941c355f",
+    "url": "/Astronaut-Manager-Client/static/js/main.4e74746e.chunk.js"
+  },
+  {
+    "revision": "3c1e30db1f78a47da9dc",
+    "url": "/Astronaut-Manager-Client/static/js/runtime-main.95d32979.js"
+  },
+  {
+    "revision": "de22e56cb25a15300e102a809f74359a",
+    "url": "/Astronaut-Manager-Client/static/media/FemaleAstronaut.de22e56c.png"
+  },
+  {
+    "revision": "df53592093b4e97c2f9eca423afcb998",
+    "url": "/Astronaut-Manager-Client/static/media/MaleAstronaut.df535920.png"
+  },
+  {
+    "revision": "7b34a834ceb67b44df594b3668572e16",
+    "url": "/Astronaut-Manager-Client/static/media/Stellar-light.7b34a834.otf"
+  }
+]);
